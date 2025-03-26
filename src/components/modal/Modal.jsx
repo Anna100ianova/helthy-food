6cd31f68744184958ebe74d03f70da3463fe2c9a
a.stoianova <bakryuanna@gmail.com>
2025-03-26 18:34:45 +0200

    add modal component

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,15 +1,18 @@
-import "./Modal.css";
+import style from "./Modal.module.css";
+import ReactDOM from "react-dom";
 
-export const Modal = () => {
-  return (
-    <div className="modal">
-      <div className="modal__dialog">
-        <div className="modal__content">
+export const Modal = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  const rendered = (
+    <div className={style.modal} onClick={(e) => e.stopPropagation()}>
+      <div className={style.modal__dialog}>
+        <div className={style.modal__content}>
+          <button data-close className={style.modal__close} onClick={onClose}>
+            &times;
+          </button>
           <form action="#">
-            <div data-close className="modal__close">
-              &times;
-            </div>
-            <div className="modal__title">
+            <div className={style.modal__title}>
               Мы свяжемся с вами как можно быстрее!
             </div>
             <input
@@ -17,19 +20,24 @@ export const Modal = () => {
               placeholder="Ваше имя"
               name="name"
               type="text"
-              className="modal__input"
+              className={style.modal__input}
             />
             <input
               required
               placeholder="Ваш номер телефона"
               name="phone"
               type="phone"
-              className="modal__input"
+              className={style.modal__input}
             />
-            <button className="btn btn_dark btn_min">Перезвонить мне</button>
+            <button
+              className={`${style.btn} ${style.btn_dark} ${style.btn_min}`}
+            >
+              Перезвонить мне
+            </button>
           </form>
         </div>
       </div>
     </div>
   );
+  return ReactDOM.createPortal(rendered, document.getElementById("modal-root"));
 };
